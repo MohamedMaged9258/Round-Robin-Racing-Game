bb3941cab75b339208cc3b6b1952119b2aff1c52
Mohamed Maged <mohamedmaged9258@gmail.com>
2024-12-22 09:37:58 +0200

    modifying obstacles

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        RoundRobinRacingGame game = new RoundRobinRacingGame(100, 10, 6);
+        RoundRobinRacingGame game = new RoundRobinRacingGame(100, 5, 3);
         game.startGame();
     }
 }
